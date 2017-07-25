f8d8b18ac994dc501b830cbcc2ca1132b3ea3406
Carlos Alberto <c.ramirez@aqtiva.mx>
2017-07-24 19:58:53 -0500

    inserto un formulario con estilos

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 var app = require('express')();//app=express();
+var express = require('express');//app=express();
 var http =require('http').createServer(app);
+var path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req,res){
-    res.send('<h1>Hola mundo!</h1>');
+    res.sendFile(__dirname+'/index.html');
 });
 
 http.listen(3000, function(){
